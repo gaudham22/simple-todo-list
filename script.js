5748040f84495568241a6dba059f14c94973137b
gaudham22 <70633016+gaudham22@users.noreply.github.com>
2023-01-14 14:12:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,32 @@
-const taskInput = document.querySelector("#task");
-const addTaskButton = document.querySelector("#add-task");
-const taskList = document.querySelector("#task-list");
+// Get the input field and the add button
+var inputField = document.getElementById("new-task");
+var addButton = document.getElementById("add-task");
+var taskList = document.getElementById("task-list");
 
-addTaskButton.addEventListener("click", function() {
-  // Get the task input value
-  const task = taskInput.value;
-  
+// Add a click event listener to the add button
+addButton.addEventListener("click", function() {
+  // Get the task text from the input field
+  var taskText = inputField.value;
+
   // Create a new list item
-  const li = document.createElement("li");
-  li.innerHTML = task;
-  
-  // Append the new task to the task list
-  taskList.appendChild(li);
-  
+  var newTask = document.createElement("li");
+  newTask.classList.add("task");
+  newTask.innerHTML = taskText;
+
+  // Create a delete button for the new task
+  var deleteButton = document.createElement("button");
+  deleteButton.classList.add("delete");
+  deleteButton.innerHTML = "Delete";
+  deleteButton.onclick = function(){
+    taskList.removeChild(newTask);
+  }
+
+  // Add the delete button to the new task
+  newTask.appendChild(deleteButton);
+
+  // Add the new task to the task list
+  taskList.appendChild(newTask);
+
   // Clear the input field
-  taskInput.value = "";
+  inputField.value = "";
 });
